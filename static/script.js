45b7159ff873cd54a246e6365b06327883e84ac4
Joshua Remlinger <pickichu@icloud.com>
2021-07-17 17:06:37 -0400

    Heavily updated styling

--- static/script.js
+++ static/script.js
@@ -1,3 +1,31 @@
+// List deco
+// let coll = document.getElementsByClassName("collapsible");
+let coll = $('.collapsible');
+for (let i = 0; i < coll.length; i++) {
+	$(coll[i]).click(function() {
+		this.classList.toggle('collactive');
+		let content = this.nextElementSibling;
+		content.style.display = (content.style.display === 'block') ? 'none' : 'block';
+	})
+}
+
+let buildWindows = async function() {
+	let response = await fetch('../static/windows/amnesia/amnesia.html');
+	let data = await response.text();
+	$('#displayzone').html(`\
+		<div class="window">\
+			<div class="windowtitlebar">\
+				<p>Window Name</p>\
+				<div class="windowclosebtn">&#10005</div>\
+			</div>\
+			<div class="windowcontent">\
+				${data}
+			</div>\
+		</div>\
+	`);
+}
+
+// Socket stuff
 let socket = io.connect('http://' + document.domain + ':' + location.port);
 
 socket.on('connect', function() {
@@ -14,3 +42,5 @@ socket.on('connect', function() {
 socket.on('from-server', function(str) {
 	console.log(str);
 })
+
+buildWindows();
